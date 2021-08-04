50a75cd2e32858ab4dd23fcb4832ebe784c515ea
Blaire Baker <bakerblaire@gmail.com>
2021-08-04 15:07:22 -0600

    build out notification seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Notification.destroy_all
+
+Notification.create(message: "Microgrant applications are now open.")
+Notification.create(message: "Congratulations! You have recieved tax exempt status.")
+Notification.create(message: "It is time to refile for your tax exempt status.")
+Notification.create(message: "You have a new message.")
+Notification.create(message: "You are currently 30% complete with your Resilia Academy course.")
