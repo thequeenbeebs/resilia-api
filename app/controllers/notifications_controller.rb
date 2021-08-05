5c8ecb01c63ba133f66e234f3f9a1664a946cb72
Blaire Baker <bakerblaire@gmail.com>
2021-08-04 18:35:21 -0600

    find notifications by username

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
     def index
-        notifications = Notification.all
+        user = User.find_by(name: params[:name])
+        notifications = Notification.select{ | n | n.user_id  == user.id }
         render json: notifications
     end
 end
