f61503b15d023fa121a04755413376ea079bc829
Blaire Baker <bakerblaire@gmail.com>
2021-08-04 18:15:06 -0600

    add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Notification.destroy_all
+User.destroy_all
 
-Notification.create(message: "Microgrant applications are now open.")
-Notification.create(message: "Congratulations! You have recieved tax exempt status.")
-Notification.create(message: "It is time to refile for your tax exempt status.")
-Notification.create(message: "You have a new message.")
-Notification.create(message: "You are currently 30% complete with your Resilia Academy course.")
+blaire = User.create(name: "Blaire")
+dinesh = User.create(name: "Dinesh")
+
+Notification.create(user_id: blaire.id, message: "Microgrant applications are now open.")
+Notification.create(user_id: blaire.id, message: "Congratulations! You have recieved tax exempt status.")
+Notification.create(user_id: blaire.id, message: "It is time to refile for your tax exempt status.")
+Notification.create(user_id: blaire.id, message: "You have a new message.")
+Notification.create(user_id: dinesh.id, message: "You are currently 30% complete with your Resilia Academy course.")
