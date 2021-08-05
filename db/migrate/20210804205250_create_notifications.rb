22efe8e1565debd506769a8ad16c7faa511eba33
Blaire Baker <bakerblaire@gmail.com>
2021-08-04 18:13:04 -0600

    add user model

--- db/migrate/20210804205250_create_notifications.rb
+++ db/migrate/20210804205250_create_notifications.rb
@@ -2,6 +2,7 @@ class CreateNotifications < ActiveRecord::Migration[6.0]
   def change
     create_table :notifications do |t|
       t.string :message
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_205250) do
+ActiveRecord::Schema.define(version: 2021_08_05_000903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "notifications", force: :cascade do |t|
     t.string "message"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
